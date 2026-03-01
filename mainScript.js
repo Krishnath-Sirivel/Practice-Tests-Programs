c0cdfe3c7db8f67a561bacc064a0d1da2598f077
Krishnath-Sirivel <krishnath.sirivel@ontariotechu.net>
2026-03-01 10:45:55 -0500

    Add buttons for midterm and exam in mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -8,6 +8,27 @@ function switchMode(cssFile, jsFile) {
     newScript.id = "logic";
     oldScript.replaceWith(newScript);
 }
+
 function hideButton(btn) {
     btn.style.display = "none";
+}
+
+function addButtons_CSCI_2050U() {
+    const container = document.getElementById("buttonContainer");
+    //midterm    
+    const midterm = document.createElement("button");
+    midterm.textContent = "Midterm";
+    midterm.onclick = function() {
+        alert("You clicked the button for the midterm for this course!");
+    };
+    //exam    
+    const exam = document.createElement("button");
+    exam.textContent = "Exam";
+    exam.onclick = function() {
+        alert("You clicked the button for the exam for this course!");
+    };
+    // Add them to the page
+    container.appendChild(midterm);
+    container.appendChild(exam);
+
 }
